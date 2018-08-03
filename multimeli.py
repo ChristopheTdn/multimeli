1bdc5b61cd8b73763a6a72c006d780478ed51521
ToF <Christophe.taudin@gmail.com>
2018-08-04 00:07:49 +0200

    Rename main en multimeli.py

---
+++ multimeli.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+#-*- coding: utf-8 -*-
+from PyQt5 import QtWidgets
+from MainUI import Form
+import sys
+global nbrFaute
+global nbrTour
+
+global myapp
+
+if __name__ == "__main__":
+      app = QtWidgets.QApplication(sys.argv)  
+      myapp = Form()
+      myapp.show()     
+      sys.exit(app.exec_())
